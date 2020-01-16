f007ceaa661db959582af7503f730bb9112816ff
Jacobs <mathew.jacobs@sinclair.edu>
2020-01-16 16:18:31 -0500

    simple models

---
+++ GenSource.API/Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenSource.API.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float[] Weights { get; set; }
+    }
+}
---
+++ GenSource.API/Models/Creature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenSource.API.Models
+{
+    public enum Typings
+    {
+
+    }
+
+    public class Creature
+    {
+        public int Id { get; set; }
+        public Animal BaseAnimal { get; set; }
+        public InanimateObject ExtraFeature { get; set; }
+        public int Type1 { get; set; }
+        public int Type2 { get; set; }
+    }
+}
